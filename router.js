a4e1abd425c99e79767838fd86e60bd64bd0f84a
Danilo Espinosa <danilopespinosa@gmail.com>
2024-05-28 12:25:14 +0200

    Actualizacion de agenda de contactos

--- router.js
+++ router.js
@@ -1,6 +1,7 @@
 // Cargar el módulo de express
 import express from 'express'
 import mysql from 'mysql'
+import path from 'path'
 
 const configConnection = {
     host: process.env.HOST,
@@ -23,7 +24,8 @@ router.get('/', (req, res) => {
 })
 
 router.get('/formulario', (req, res) => {
-    res.send('Seré dinámico algún día')
+    res.render('formulario', {title: 'Añadir contacto', id: 0})
+    // res.send('Seré dinámico algún día')
 })
 
 router.post('/insert', (req, res) => {
@@ -35,6 +37,34 @@ router.post('/insert', (req, res) => {
     })
     // console.log(req.body)
     // res.send('Seré dinámico algún día') esto generaba el error
+})
+
+router.get('/borrar/:id', (req, res) => {
+    const { id } = req.params
+    const borrarLinea  = `DELETE FROM agenda WHERE id = ${id}`
+    connection.query(borrarLinea, (err, result, fields) => {
+        if (err) throw err
+        res.redirect('/')
+    })
+})
+
+router.get('/editar/:id/:nombre/:apellido/:telefono/:tipo', (req, res) => {
+    // console.log(req.params) devuelve los valores
+    const contacto = {
+        ...req.params
+    }
+    // console.log(contacto)
+    res.render('formulario', { title: "Editar Contacto", id: 1, contacto: contacto})
+})
+
+
+router.post('/update', (req, res) => {
+    const {id, nombre, apellido, tel, tipo} = req.body
+    const update = `UPDATE agenda SET nombre = '${nombre}', apellido = '${apellido}', telefono = '${tel}', tipo = '${tipo}' WHERE id = '${id}'`;
+    connection.query(update, (err, result, fields) => {
+        if (err) throw err
+        res.redirect('/')
+    })
 })
 
 export default router 
